869daf304f5714d4de9dcced0cbc8c8ad3b89652
inkzhi <king.china.dev@gmail.com>
2016-11-03 22:10:50 +0800

    Nov.3

---
+++ LeetCode/036. Valid Sudoku.c
@@ -0,0 +1,22 @@
+bool isValidSudoku(char** board, int boardRowSize, int boardColSize) {
+	int i, j, c;
+	char row[9][9], col[9][9], block[3][3][9];
+	memset(row, 0, sizeof(row));
+	memset(col, 0, sizeof(col));
+	memset(block, 0, sizeof(block));
+	for (i = 0; i < 9; i++) {
+		for (j = 0; j < 9; j++) {
+			if(board[i][j] != '.'){
+				c = board[i][j] - '1';
+				if (row[i][c] || col[j][c] || block[i / 3][j / 3][c])
+					return false;
+				else {
+					row[i][c] ++;
+					col[j][c] ++;
+					block[i / 3][j / 3][c]++;
+				}
+			}
+		}
+	}
+	return true;
+}
---
+++ POJ/1723 SOLDIERS.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+int x[10000], y[10000];
+
+int main() {
+	int n;
+	int cnt = 0;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) scanf("%d%d", &x[i], &y[i]);
+	sort(x, x + n);
+	sort(y, y + n);
+	for (int i = 0; i < n; i++) x[i] -= i;
+	sort(x, x + n);
+	for (int i = 0; i < n / 2; i++) cnt += (x[n - 1 - i] - x[i]) + (y[n - 1 - i] - y[i]);
+	printf("%d\n", cnt);
+	return 0;
+}
