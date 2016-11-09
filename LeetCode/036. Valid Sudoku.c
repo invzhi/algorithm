949b3935e0d071a9b54e36b0525d01e9dbe86dea
inkzhi <king.china.dev@gmail.com>
2016-11-09 22:38:58 +0800

    Nov.9

--- LeetCode/036. Valid Sudoku.c
+++ LeetCode/036. Valid Sudoku.c
@@ -1,6 +1,6 @@
 bool isValidSudoku(char** board, int boardRowSize, int boardColSize) {
 	int i, j, c;
-	char row[9][9], col[9][9], block[3][3][9];
+	bool row[9][9], col[9][9], block[9][9];
 	memset(row, 0, sizeof(row));
 	memset(col, 0, sizeof(col));
 	memset(block, 0, sizeof(block));
@@ -8,13 +8,8 @@ bool isValidSudoku(char** board, int boardRowSize, int boardColSize) {
 		for (j = 0; j < 9; j++) {
 			if(board[i][j] != '.'){
 				c = board[i][j] - '1';
-				if (row[i][c] || col[j][c] || block[i / 3][j / 3][c])
-					return false;
-				else {
-					row[i][c] ++;
-					col[j][c] ++;
-					block[i / 3][j / 3][c]++;
-				}
+				if (row[i][c] || col[j][c] || block[i / 3 * 3 + j / 3][c]) return false;
+				else row[i][c] = col[j][c] = block[i / 3 * 3 + j / 3][c] = true;
 			}
 		}
 	}
---
+++ LeetCode/037. Sudoku Solver.c
@@ -0,0 +1,35 @@
+bool row[9][9], col[9][9], block[9][9];
+int cnt = 0;
+
+bool DFS(char** board, int i, int j) {
+	if (i == 9) return true;
+	if (j == 9) return DFS(board, i + 1, 0);
+	if (board[i][j] != '.') return DFS(board, i, j + 1);
+	for (int k = 0; k < 9; k++) {
+		if (!row[i][k] && !col[j][k] && !block[i / 3 * 3 + j / 3][k]) {
+			row[i][k] = col[j][k] = block[i / 3 * 3 + j / 3][k] = true;
+			board[i][j] = k + '1';
+			if (DFS(board, i, j + 1)) return true;
+			board[i][j] = '.';
+			row[i][k] = col[j][k] = block[i / 3 * 3 + j / 3][k] = false;
+		}
+	}
+	return false;
+}
+
+void solveSudoku(char** board, int boardRowSize, int boardColSize) {
+	int i, j, c;
+	memset(row, 0, sizeof(row));
+	memset(col, 0, sizeof(col));
+	memset(block, 0, sizeof(block));
+	for (i = 0; i < 9; i++) {
+		for (j = 0; j < 9; j++) {
+			if (board[i][j] == '.') cnt++;
+			else {
+				c = board[i][j] - '1';
+				row[i][c] = col[j][c] = block[i / 3 * 3 + j / 3][c] = true;
+			}
+		}
+	}
+	DFS(board, 0, 0);
+}
