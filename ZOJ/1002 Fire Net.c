81fd9aef11a790655d5b83dc408661a0ea12c6f1
inkzhi <king.china.dev@gmail.com>
2016-12-15 18:04:23 +0800

    Dec.15

---
+++ ZOJ/1002 Fire Net.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int n, max;
+char map[5][5];
+
+int isPlace(int x, int y) {
+	if (map[x][y] != '.') return 0;
+	int i;
+	for (i = x - 1; i >= 0; i--) {
+		if (map[i][y] == 'X') break;
+		if (map[i][y] == 'b') return 0;
+	}
+	for (i = y - 1; i >= 0; i--) {
+		if (map[x][i] == 'X') break;
+		if (map[x][i] == 'b') return 0;
+	}
+	return 1;
+}
+
+void DFS(int cur, int pos) {
+	if (pos == n * n) {
+		if (cur > max) max = cur;
+		return;
+	}
+	int i = pos / n, j = pos % n;
+	if (isPlace(i, j)) {
+		map[i][j] = 'b';
+		DFS(cur + 1, pos + 1);
+		map[i][j] = '.';
+	}
+	DFS(cur, pos + 1);
+}
+
+int main() {
+	while (scanf("%d", &n) == 1 && n) {
+		int i;
+		for (i = 0; i < n; i++) scanf("%s", map[i]);
+		max = 0;
+		DFS(0, 0);
+		printf("%d\n", max);
+	}
+	return 0;
+}
---
+++ ZOJ/1008 Gnome Tetravex.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+
+int n, num, kind;
+int square[25][4];
+int numOfSquare[25];
+int ans[25];
+
+int DFS(int cur) {
+	if (cur == num) return 1;
+	int i;
+	for (i = 0; i < kind; i++) {
+		if (numOfSquare[i] == 0) continue;
+		if (cur / n && square[ans[cur - n]][2] != square[i][0]) continue;
+		if (cur % n && square[ans[cur - 1]][1] != square[i][3]) continue;
+		ans[cur] = i;
+		numOfSquare[i]--;
+		if (DFS(cur + 1)) return 1;
+		numOfSquare[i]++;
+	}
+	return 0;
+}
+
+int main() {
+	int i, j, a, b, c, d, first = 1, k = 0;
+	while (scanf("%d", &n) && n) {
+		kind = 0;
+		num = n * n;
+		memset(numOfSquare, 0, sizeof(numOfSquare));
+		for (i = 0; i < num; i++) {
+			scanf("%d%d%d%d", &a, &b, &c, &d);
+			for (j = kind - 1; j >= 0; j--) {
+				if (a == square[j][0] && b == square[j][1] && c == square[j][2] && d == square[j][3]) {
+					numOfSquare[j]++;
+					break;
+				}
+			}
+			if (j == -1) {
+				square[kind][0] = a;
+				square[kind][1] = b;
+				square[kind][2] = c;
+				square[kind][3] = d;
+				numOfSquare[kind]++;
+				kind++;
+			}
+		}
+		if (first) first = 0;
+		else printf("\n");
+		if (DFS(0)) printf("Game %d: Possible\n", ++k);
+		else printf("Game %d: Impossible\n", ++k);
+	}
+	return 0;
+}
