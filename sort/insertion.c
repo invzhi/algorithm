298c1a152b5fdea4f9a90383cc6bc48a136dd598
invzhi <invzhi@protonmail.com>
2017-09-20 15:39:50 +0800

    Optimized insertion sort

--- sort/insertion.c
+++ sort/insertion.c
@@ -4,10 +4,19 @@
 #define N 20
 
 void insertion_sort(int a[], int length) {
+	int exchanges = 0;
+	for (int i = length - 1; i > 0; i--) {
+		if (a[i] < a[i - 1]) {
+			exch(&a[i], &a[i - 1]);
+			exchanges++;
+		}
+	}
+	if (exchanges == 0) return;
+
 	int j, t;
 	for (int i = 1; i < length; i++) {
 		t = a[i];
-		for (j = i; j > 0 && a[j - 1] > t; j--) {
+		for (j = i; a[j - 1] > t; j--) {
 			a[j] = a[j - 1];
 		}
 		a[j] = t;
