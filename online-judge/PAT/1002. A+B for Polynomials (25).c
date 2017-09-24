9021b2c6a0c4daf27823d9c23dcb350f03879643
invzhi <invzhi@protonmail.com>
2017-09-24 14:01:40 +0800

    Add 1002 in PAT

---
+++ online-judge/PAT/1002. A+B for Polynomials (25).c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+// Sample Input
+// K N1 aN1 N2 aN2 ... NK aNK
+// 2 1 2.4 0 3.2
+// 2 2 1.5 1 0.5
+// Sample Output
+// 3 2 1.5 1 2.9 0 3.2
+
+// 1 <= K <= 10，0 <= NK < ... < N2 < N1 <=1000.
+
+#define N 1001
+
+int main() {
+	float map[N];
+	int m, n, t;
+	float num;
+	memset(map, 0, sizeof(float) * N);
+
+	scanf("%d", &m);
+	for (int i = 0; i < m; i++) {
+		scanf("%d%f", &t, &num);
+		map[t] += num;
+	}
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d%f", &t, &num);
+		map[t] += num;
+	}
+	int cnt = 0;
+	for (int i = 0; i < N; i++) {
+		if (map[i] != 0.0) {
+			cnt++;
+		}
+	}
+	printf("%d", cnt);
+	for (int i = N - 1; i >= 0; i--) {
+		if (map[i] != 0.0) {
+			printf(" %d %.1f", i, map[i]);
+		}
+	}
+	return 0;
+}
