4fedb43e8c136a7af672dffeb67a238189c698f5
invzhi <invzhi@protonmail.com>
2017-10-30 20:52:21 +0800

    Add PAT basic 1007

---
+++ online-judge/PAT/basic/1007. 素数对猜想 (20).c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void eratosthenes(int primes[], int n) {
+	for (int i = 0; i <= n; i++) {
+		primes[i] = i;
+	}
+	for (int p = 2; p*p <= n; p++) {
+		if (primes[p]) {
+			for (int i = p+p; i <= n; i += p) {
+				primes[i] = 0;
+			}
+		}
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	int n;
+	scanf("%d", &n);
+
+	int couple = 0;
+	int primes[n+1];
+	eratosthenes(primes, n);
+	for (int i = 3; i+2 <= n; i += 2) {
+		if (primes[i] > 1 && primes[i+2] > 1) {
+			couple++;
+		}
+	}
+	printf("%d\n", couple);
+	return 0;
+}
