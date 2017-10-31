493a8cf897edc25c077a871c0e4fc3a986f78324
invzhi <invzhi@protonmail.com>
2017-10-31 12:58:34 +0800

    Add PAT basic 1010

---
+++ online-judge/PAT/basic/1010. 一元多项式求导 (25).c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define N 100
+
+int main(int argc, char const *argv[])
+{
+	int a, n;
+	int first = 1;
+	while (scanf("%d %d", &a, &n) == 2) {
+		if (n != 0) {
+			printf(first ? "%d %d" : " %d %d", a*n, n-1);
+			first = 0;
+		}
+	}
+	if (first) {
+		printf("0 0");
+	}
+	return 0;
+}
