0e9f4e5720d688ac9fd2660869b27feb44793fb6
invzhi <invzhi@protonmail.com>
2017-11-01 11:53:17 +0800

    Add PAT basic 1016

---
+++ online-judge/PAT/basic/1016. 部分A+B (15).c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#define N 11
+
+int p(char s[], char c) {
+	int cnt = 0;
+	for (int i = 0; s[i] != '\0'; i++) {
+		if (s[i] == c) {
+			cnt++;
+		}
+	}
+	int num = 0;
+	for (int i = 0; i < cnt; i++) {
+		num = num*10 + (c - '0');
+	}
+	return num;
+}
+
+int main(int argc, char const *argv[])
+{
+	char a[N], b[N];
+	char da, db;
+	scanf("%s %c %s %c", a, &da, b, &db);
+
+	int sum = p(a, da) + p(b, db);
+	printf("%d", sum);
+	return 0;
+}
