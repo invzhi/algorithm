e604ce8284433412e8b6becbfe9b6d4ffacb8e22
invzhi <invzhi@protonmail.com>
2017-11-02 21:14:45 +0800

    Optimized PAT basic 1002

--- online-judge/PAT/basic/1002. 写出这个数 (20).c
+++ online-judge/PAT/basic/1002. 写出这个数 (20).c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 
-#define N 101
-
 // 1234567890987654321123456789 -> yi san wu
 
 int main(int argc, char const *argv[])
 {
-	char n[N];
-	scanf("%s", n);
-
+	char c;
 	int sum = 0;
-	for (int i = 0; n[i] != '\0'; i++) {
-		sum += n[i] - '0';
+	while (((c = getchar()) != '\n')) {
+		sum += c - '0';
 	}
 
 	char s[4]; // max: 900
