238646ae8e0f8282bb230aa016fcb15a5ad5b694
invzhi <invzhi@protonmail.com>
2017-11-03 08:58:28 +0800

    Add PAT basic 1021

---
+++ online-judge/PAT/basic/1021. 个位数统计 (15).c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+	char c;
+	int cnt[10];
+	memset(cnt, 0, sizeof(cnt));
+	while ((c = getchar()) != '\n') {
+		cnt[c-'0']++;
+	}
+
+	for (int i = 0; i < 10; i++) {
+		if (cnt[i] > 0) {
+			printf("%d:%d\n", i, cnt[i]);
+		}
+	}
+	return 0;
+}
