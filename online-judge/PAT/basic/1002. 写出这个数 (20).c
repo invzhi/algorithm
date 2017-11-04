e29a69544ac770487356fd46e6fa94d5fcb323a1
invzhi <invzhi@protonmail.com>
2017-11-04 19:42:34 +0800

    Modify PAT basic 1002 for robust

--- online-judge/PAT/basic/1002. 写出这个数 (20).c
+++ online-judge/PAT/basic/1002. 写出这个数 (20).c
@@ -6,12 +6,12 @@ int main(int argc, char const *argv[])
 {
 	char c;
 	int sum = 0;
-	while (((c = getchar()) != '\n')) {
+	while (((c = getchar()) != '\n' && c != EOF)) {
 		sum += c - '0';
 	}
 
-	char s[4]; // max: 900
 	char* pinyin[] = {"ling", "yi", "er", "san", "si", "wu", "liu", "qi", "ba", "jiu"};
+	char s[4]; // max: 900
 	sprintf(s, "%d", sum);
 	for (int i = 0; s[i] != '\0'; i++) {
 		printf(i ? " %s" : "%s", pinyin[s[i]-'0']);
