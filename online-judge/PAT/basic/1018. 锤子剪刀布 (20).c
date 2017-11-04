ca4e9239a28504c9946bda0e74cebac60d5ee18d
invzhi <invzhi@protonmail.com>
2017-11-04 20:50:34 +0800

    Optimize PAT basic 1018

--- online-judge/PAT/basic/1018. 锤子剪刀布 (20).c
+++ online-judge/PAT/basic/1018. 锤子剪刀布 (20).c
@@ -17,15 +17,15 @@ int tonum(char c) {
 	return num;
 }
 
-int maxi(int a[], int n) {
-	int max = 0, max_i = 0;
-	for (int i = 0; i < n; i++) {
-		if (a[i] > max) {
-			max = a[i];
-			max_i = i;
-		}
-	}
-	return max_i;
+char max3(int a[]) {
+	char tobcj[] = {'B', 'C', 'J'};
+	int res = a[0] >= a[1] ? 0 : 1;
+	res = a[res] >= a[2] ? res : 2;
+	return tobcj[res];
+}
+
+int sum(int a[]) {
+	return a[0] + a[1] + a[2];
 }
 
 int main(int argc, char const *argv[])
@@ -33,9 +33,7 @@ int main(int argc, char const *argv[])
 	int n;
 	scanf("%d", &n);
 
-	int cnt[2][3]; // win, draw, lose
-	int bcj[2][3]; // win time for B, C, J
-	memset(cnt, 0, sizeof(cnt));
+	int bcj[2][3]; // win times for B, C, J
 	memset(bcj, 0, sizeof(bcj));
 
 	for (int i = 0; i < n; i++) {
@@ -43,34 +41,20 @@ int main(int argc, char const *argv[])
 		scanf(" %c %c", &a, &b);
 
 		int an = tonum(a), bn = tonum(b);
-		int d = bn - an;
-		switch (d) {
+		switch (bn - an) {
 			case 1:
 			case -2:
-				cnt[0][0]++; // a win
-				cnt[1][2]++; // b lose
 				bcj[0][an]++;
 				break;
 			case -1:
 			case 2:
-				cnt[1][0]++; // b win
-				cnt[0][2]++; // a lose
 				bcj[1][bn]++;
-				break;
-			default:
-				cnt[0][1]++; // a draw
-				cnt[1][1]++; // b draw
 				break;
 		}
 	}
 
-	for (int i = 0; i < 2; i++) {
-		printf("%d %d %d\n", cnt[i][0], cnt[i][1], cnt[i][2]);
-	}
-
-	char tobcj[] = {'B', 'C', 'J'};
-	int maxai = maxi(bcj[0], 3);
-	int maxbi = maxi(bcj[1], 3);
-	printf("%c %c", tobcj[maxai], tobcj[maxbi]);
+	int win = sum(bcj[0]), lose = sum(bcj[1]), draw = n - win - lose; // a
+	printf("%d %d %d\n%d %d %d\n", win, draw, lose, lose, draw, win);
+	printf("%c %c", max3(bcj[0]), max3(bcj[1]));
 	return 0;
 }
