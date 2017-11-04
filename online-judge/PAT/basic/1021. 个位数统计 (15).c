f8e155721982f04f06b5d74c9a4befe07fa283fa
invzhi <invzhi@protonmail.com>
2017-11-04 20:56:19 +0800

    Modify PAT basic 1021 for robust

--- online-judge/PAT/basic/1021. 个位数统计 (15).c
+++ online-judge/PAT/basic/1021. 个位数统计 (15).c
@@ -6,7 +6,7 @@ int main(int argc, char const *argv[])
 	char c;
 	int cnt[10];
 	memset(cnt, 0, sizeof(cnt));
-	while ((c = getchar()) != '\n') {
+	while ((c = getchar()) != '\n' && c != EOF) {
 		cnt[c-'0']++;
 	}
 
