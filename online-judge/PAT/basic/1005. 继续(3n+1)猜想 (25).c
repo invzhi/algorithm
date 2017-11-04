71cdcfbe0e071b8498c5cd8c5bd7aac4f4369c34
invzhi <invzhi@protonmail.com>
2017-11-04 18:48:02 +0800

    Optimize PAT basic 1005

--- online-judge/PAT/basic/1005. 继续(3n+1)猜想 (25).c
+++ online-judge/PAT/basic/1005. 继续(3n+1)猜想 (25).c
@@ -1,35 +1,50 @@
 #include <stdio.h>
 #include <string.h>
 
-#define N 4617
+#define N 101
 
 int main(int argc, char const *argv[])
 {
-	int k;
+	int k; // < 100
 	scanf("%d", &k);
 
-	char num[N];
-	memset(num, 0, sizeof(num));
+	char mark[N];
+	memset(mark, 0, sizeof(mark));
 	for (int i = 0; i < k; i++) {
-		int n;
+		int n; // 1 < n <= 100
 		scanf("%d", &n);
+		mark[n] = 1;
+	}
 
-		num[n]++;
-		while (n != 1) {
-			if (n & 1) {
-				n += n/2 + 1;
-			} else {
-				n /= 2;
+	for (int i = 2; i < N; i++) {
+		if (mark[i] == 1) {
+			int n = i;
+			while (n != 1) {
+				if (n & 1) {
+					n += n/2 + 1; // n = (3*n + 1) / 2
+				} else {
+					n /= 2;
+				}
+				if (n < N) {
+					if (mark[n] != -1) {
+						mark[n] = -1;
+					} else {
+						break;
+					}
+				}
 			}
-			num[n]--;
 		}
 	}
 
-	int first = 1;
-	for (int i = N - 1; i > 1; i--) {
-		if (num[i] == 1) {
-			printf(first ? "%d" : " %d", i);
-			first = 0;
+	int i = N - 1;
+	while (mark[i] != 1) {
+		i--;
+	}
+	printf("%d", i);
+
+	for (i = i - 1; i > 1; i--) {
+		if (mark[i] == 1) {
+			printf(" %d", i);
 		}
 	}
 	return 0;
