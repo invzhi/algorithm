b90738d1d6da5b52868ccd3e523d1948ee827c49
invzhi <invzhi@protonmail.com>
2017-11-04 20:03:49 +0800

    Modify PAT basic 1013 for more readable

--- online-judge/PAT/basic/1013. 数素数 (20).c
+++ online-judge/PAT/basic/1013. 数素数 (20).c
@@ -4,23 +4,24 @@
 
 int main(int argc, char const *argv[])
 {
-	int m, n;
+	int m, n; // m <= n <= 10000
 	scanf("%d %d", &m, &n);
 
 	int primes[N] = {2, 3};
-	int j = 2;
-	for (int p = 5; j < n; p += 2) {
+	int p = 2;
+	for (int num = 5; p < n; num += 2) {
 		int is_prime = 1;
-		for (int i = 1; primes[i]*primes[i] <= p; i++) {
-			if (p % primes[i] == 0) {
+		for (int i = 1; primes[i]*primes[i] <= num; i++) {
+			if (num % primes[i] == 0) {
 				is_prime = 0;
 				break;
 			}
 		}
 		if (is_prime) {
-			primes[j++] = p;
+			primes[p++] = num;
 		}
 	}
+
 	int l = n - m;
 	for (int i = 0; i < l; i++) {
 		printf("%d", primes[m-1+i]);
