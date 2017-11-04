5feadeebbbfdb758e5cf225bd524f40e41f9ca3d
invzhi <invzhi@protonmail.com>
2017-11-04 19:19:24 +0800

    Optimize PAT basic 1008

--- online-judge/PAT/basic/1008. 数组元素循环右移问题 (20).c
+++ online-judge/PAT/basic/1008. 数组元素循环右移问题 (20).c
@@ -4,16 +4,19 @@ int main(int argc, char const *argv[])
 {
 	int n, m; // 1 <= n <= 100, m >= 0
 	scanf("%d %d", &n, &m);
+	m %= n;
 
 	int num[n];
-	for (int i = 0; i < n; i++) {
-		int index = (i + m) % n;
-		scanf("%d", &num[index]);
+	for (int i = m; i < n; i++) {
+		scanf("%d", &num[i]);
+	}
+	for (int i = 0; i < m; i++) {
+		scanf("%d", &num[i]);
 	}
 
-	for (int i = 0; i < n - 1; i++) {
-		printf("%d ", num[i]);
+	printf("%d", num[0]);
+	for (int i = 1; i < n; i++) {
+		printf(" %d", num[i]);
 	}
-	printf("%d", num[n-1]);
 	return 0;
 }
