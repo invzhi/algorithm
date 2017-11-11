84e7f748ee95c0026005fcce7600fe0d87fe24e9
invzhi <invzhi@protonmail.com>
2017-11-11 19:02:59 +0800

    Add PAT basic 1038

---
+++ online-judge/PAT/basic/1038. 统计同成绩学生(20).c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+	int n;
+	scanf("%d", &n);
+
+	int cnt[101];
+	memset(cnt, 0, sizeof(cnt));
+	for (int i = 0; i < n; i++) {
+		int score;
+		scanf("%d", &score);
+		cnt[score]++;
+	}
+
+	int k;
+	scanf("%d", &k);
+	for (int i = 0; i < k; i++) {
+		int score;
+		scanf("%d", &score);
+		printf(i == k - 1 ? "%d" : "%d ", cnt[score]);
+	}
+
+	return 0;
+}
