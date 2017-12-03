18324fc05b55e3b0252e916466bf927dbe0f7f49
invzhi <invzhi@protonmail.com>
2017-12-03 19:57:35 +0800

    Optimize PAT advanced 1005

--- online-judge/PAT/advanced/1005. Spell It Right (20).c
+++ online-judge/PAT/advanced/1005. Spell It Right (20).c
@@ -1,31 +1,27 @@
 #include <stdio.h>
-#include <string.h>
 
 // Sample Input:
 // 12345 (N <= 10^100)
 // Sample Output:
 // one five
 
-#define N 101
-
 int main(int argc, char const *argv[])
 {
-	char* num[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+	char* toword[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 	
-	char n[N], res[N];
 	int sum = 0;
-
-	scanf("%s", n);
-	int len = strlen(n);
-	for (int i = 0; i < len; i++) {
-		sum += n[i] - '0';
+	char c;
+	while ((c = getchar()) != '\n' && c != EOF) {
+		sum += c - '0';
 	}
 
-	len = sprintf(res, "%d", sum);
-	printf("%s", num[res[0] - '0']);
-	for (int i = 1; i < len; i++) {
-		int index = res[i] - '0';
-		printf(" %s", num[index]);
+	char res[4]; // max: 892
+	sprintf(res, "%d", sum);
+
+	printf("%s", toword[res[0]-'0']);
+	for (int i = 1; res[i] != '\0'; i++) {
+		printf(" %s", toword[res[i]-'0']);
 	}
+
 	return 0;
 }
